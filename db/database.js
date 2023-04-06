4cada3416e02160422cc3124645084adbe51197e
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-06 09:28:57 -0700

    Finish login queries

---
+++ db/database.js
@@ -0,0 +1,108 @@
+const { Pool } = require('pg');
+const properties = require('./json/properties.json');
+
+const pool = new Pool({
+  user: 'postgres',
+  password: 'password',
+  host: '127.0.0.1',
+  database: 'lightbnb',
+});
+
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithEmail = function (email) {
+  return pool
+    .query('SELECT * FROM users WHERE email=$1', [email])
+    .then((res) => {
+      return res.rows[0]
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+/**
+ * Get a single user from the database given their id.
+ * @param {string} id The id of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithId = function (id) {
+  return pool
+    .query('SELECT * FROM users WHERE id=$1', [id])
+    .then((res) => {
+      console.log(res.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+/**
+ * Add a new user to the database.
+ * @param {{name: string, password: string, email: string}} user
+ * @return {Promise<{}>} A promise to the user.
+ */
+const addUser = function (user) {
+  return pool
+    .query('INSERT INTO users (name, password, email) VALUES ($1, $2, $3)', [user.name, user.password, user.email])
+    .then((res) => {
+      console.log(res.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+/// Reservations
+
+/**
+ * Get all reservations for a single user.
+ * @param {string} guest_id The id of the user.
+ * @return {Promise<[{}]>} A promise to the reservations.
+ */
+const getAllReservations = function (guest_id, limit = 10) {
+  return getAllProperties(null, 2);
+};
+
+/// Properties
+
+/**
+ * Get all properties.
+ * @param {{}} options An object containing query options.
+ * @param {*} limit The number of results to return.
+ * @return {Promise<[{}]>}  A promise to the properties.
+ */
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+/**
+ * Add a property to the database
+ * @param {{}} property An object containing all of the property details.
+ * @return {Promise<{}>} A promise to the property.
+ */
+const addProperty = function (property) {
+  const propertyId = Object.keys(properties).length + 1;
+  property.id = propertyId;
+  properties[propertyId] = property;
+  return Promise.resolve(property);
+};
+
+module.exports = {
+  getUserWithEmail,
+  getUserWithId,
+  addUser,
+  getAllReservations,
+  getAllProperties,
+  addProperty,
+};
